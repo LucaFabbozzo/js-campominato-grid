2be199b4425c59782fc521fc3fedbb3c31431a5e
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-20 11:35:08 -0500

    Last

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-// Esercizio di oggi: **Griglia Campo Minato**
-// nome repo: **`js-campominato-grid`**
-// **Consegna**
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
@@ -10,12 +7,7 @@
 // - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-// **Consigli del giorno:**  :party_wizard:
-// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio:
-// Di cosa ho bisogno per generare i numeri?
-// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-// Le validazioni e i controlli possiamo farli anche in un secondo momento.
+
 
 
 // Macroaree;
@@ -25,19 +17,13 @@
 // 4 Gestire il click del quadratino
 
 
-
-// Facciamo apparire i quadratini dinamicamente con js
-
 const container = document.querySelector('.container');
 let squareForRow;
-
 
 // select levels
 const select = document.querySelector('[name="levels"]');
 
 
-//creo una funzione per far partire tutto e far stampare i quadratini e per poter calcolare gli squares totali
-// aggungo un bottone che fa partire tutto al click
 document.getElementById('start').addEventListener('click', function(){
   squareForRow = document.querySelector('[name="levels"]').value;
   container.innerHTML = '';
@@ -48,17 +34,15 @@ document.getElementById('start').addEventListener('click', function(){
 function init(numElements) {
   const totalSquares = numElements * numElements;
   for(let i = 0; i < totalSquares; i++) {
-    //funzione per creare quadratini
+ 
     createSquare(i)
   }
   
 }
 
-//creo una funzione per creare quadratini
+
 function createSquare(idSquare) {
-  //creo il quadratino
-  //gli do un valore
-  //lo appendo al container
+
   const square = document.createElement('div');
   square.className = 'square';
   square.innerText = idSquare + 1;
